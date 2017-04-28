9cf9cbda013bbc7fd2f3e5f7b05ef8251b5f86de
João Serra <cybernabojr@gmail.com>
2017-04-28 13:20:54 +0100

    Add CreateShorty and ShortcodeGenerator interactors
    `CreateShorty` interactor is responsible for creating shorties
    in the database, provided a `url` and an (optional) `shortcode`.
    
    When the shortcode is not provided it uses `ShortcodeGenerator` to
    generate a random shortcode based on the regex `/^[0-9a-zA-Z_]{6}$/`

---
+++ lib/shorty/interactors/shortcode_generator.rb
@@ -0,0 +1,34 @@
+require 'shorty/repositories/shorties_repository'
+require 'faker'
+
+module Shorty
+  module Interactors
+    class ShortcodeGenerator
+      RANDOM_SHORTCODE_REGEX = /^[0-9a-zA-Z_]{6}$/
+
+      attr_reader :shorties_repository
+      attr_reader :generator
+
+      def initialize(environment)
+        @shorties_repository = environment[:shorties_repository]
+        @generator = environment[:faker]
+      end
+
+      def call
+        generate_unique_shortcode(random_shortcode)
+      end
+
+      def generate_unique_shortcode(shortcode)
+        while shorties_repository.find_by_shortcode shortcode
+          shortcode = random_shortcode
+        end
+
+        shortcode
+      end
+
+      def random_shortcode
+        generator.regexify(RANDOM_SHORTCODE_REGEX)
+      end
+    end
+  end
+end
---
+++ spec/unit/interactors/shortcode_generator_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+require 'shorty/interactors/shortcode_generator'
+require 'shorty/repositories/shorties_repository'
+require 'shorty/interactors/find_shorty'
+require 'support/shorties'
+require 'faker'
+
+RSpec.describe Shorty::Interactors::ShortcodeGenerator do
+  SHORTCODE_REGEX = /^[0-9a-zA-Z_]{6}$/
+
+  subject(:shortcode_generator) do
+    described_class.new(
+      shorties_repository: shorties_repository,
+      faker: faker
+    )
+  end
+
+  let(:shorties_repository) do
+    instance_double(Shorty::ShortiesRepository)
+  end
+
+  let(:faker) do
+    Faker::Base
+  end
+
+  let(:shorty_as_entity) do
+    Support::Shorties.new_shorty
+  end
+
+  let(:shortcode) do
+    subject.call
+  end
+
+  context '#call' do
+    context 'when no shortcodes exist' do
+      it 'returns a valid shortcode' do
+        expect(shorties_repository)
+          .to receive(:find_by_shortcode)
+          .and_return(false)
+        expect(faker)
+          .to receive(:regexify)
+          .with(SHORTCODE_REGEX)
+          .and_call_original
+
+        expect(shortcode).to match SHORTCODE_REGEX
+      end
+    end
+
+    context 'when the shortcode exists' do
+      let(:existing_code) { shorty_as_entity.shortcode }
+      let(:new_code) { 'fake_2' }
+
+      it 'attemps to generate a new shortcode' do
+        expect(shorties_repository)
+          .to receive(:find_by_shortcode)
+          .and_return(shorty_as_entity, false)
+        expect(faker)
+          .to receive(:regexify)
+          .with(SHORTCODE_REGEX)
+          .and_return(existing_code, new_code)
+
+        expect(shortcode).not_to eq existing_code
+        expect(shortcode).to match SHORTCODE_REGEX
+      end
+    end
+  end
+end
