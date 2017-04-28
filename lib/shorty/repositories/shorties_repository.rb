ccb495b01dedb5d9db0692a0d27e4ed842c8ecab
João Serra <cybernabojr@gmail.com>
2017-04-28 13:00:01 +0100

    Add findShorty interactor
    New interactor resposible to to interact with the shorties
    repository and retrieve shorties by their shortcode.
    
    Returns shorty when it's found or `ShortyNotFoundError` when
    there is no shorty in the database with the requested `shortcode`.
    
    Also, and because we want to count all the access to each shorty,
    it uses a specific method in the repository that increments the
    shorty `access_count` attribute.

---
+++ lib/shorty/repositories/shorties_repository.rb
@@ -0,0 +1,30 @@
+require 'mongoid'
+
+module Shorty
+  class ShortiesRepository
+    def entity_class
+      Shorty
+    end
+
+    def create(entity)
+      return entity.reload if entity.save
+      false
+    end
+
+    def find_and_update_shortcode(shortcode)
+      entity = find_by_shortcode shortcode
+      return false unless entity
+
+      entity.access_count += 1
+      entity.save
+
+      entity
+    end
+
+    def find_by_shortcode(shortcode)
+      Shorty.find_by(shortcode: shortcode)
+    rescue Mongoid::Errors::DocumentNotFound
+      return false
+    end
+  end
+end
